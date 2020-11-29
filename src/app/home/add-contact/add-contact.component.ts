62f456ff80e00940bb48488df52f76820df64b6b
Laércio Oliveira <jose.laercio.bezerra@gmail.com>
2020-11-29 07:18:26 -0300

    feat: add add-contact-modal

--- src/app/home/add-contact/add-contact.component.ts
+++ src/app/home/add-contact/add-contact.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { ThrowStmt } from '@angular/compiler';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-add-contact',
   templateUrl: './add-contact.component.html',
-  styleUrls: ['./add-contact.component.scss']
+  styleUrls: ['./add-contact.component.scss'],
 })
 export class AddContactComponent implements OnInit {
+  @Input() showModal: boolean;
+  @Output() closeModal = new EventEmitter<boolean>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  close() {
+    this.closeModal.emit(false);
   }
-
 }
--- src/app/home/contact-list/contact-list.component.ts
+++ src/app/home/contact-list/contact-list.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contact-list',
   templateUrl: './contact-list.component.html',
-  styleUrls: ['./contact-list.component.scss']
+  styleUrls: ['./contact-list.component.scss'],
 })
 export class ContactListComponent implements OnInit {
+  showModal: boolean;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  openModal(): void {
+    this.showModal = true;
   }
 
+  closeModal(value: boolean) {
+    this.showModal = value;
+  }
 }
